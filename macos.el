38d507cfe3f8c1104d34515869a2b872c3a8919a
Pablo Stafforini <pablo@stafforini.com>
2024-03-14 14:02:31 +0100

    Add when-macos macro and use it in all macos funs

--- macos.el
+++ macos.el
@@ -46,14 +46,20 @@ Run `blueutil --paired' to see paired devices"
 
 ;;;; Functions
 
+(defmacro when-macos (&rest body)
+  "Run BODY only if `system-type' is darwin."
+  `(when (eq system-type 'darwin)
+     ,@body))
+
 ;;;;; homebrew
 
 (defun macos-update-homebrew ()
   "Update Homebrew."
   (interactive)
-  (let ((shell-command-buffer-name-async "*homebrew update*"))
-    (async-shell-command "brew update; brew upgrade --greedy; brew cleanup; brew doctor")
-    (message "Update process finished.")))
+  (when-macos
+   (let ((shell-command-buffer-name-async "*homebrew update*"))
+     (async-shell-command "brew update; brew upgrade --greedy; brew cleanup; brew doctor")
+     (message "Update process finished."))))
 
 ;;;;; bluetooth
 
@@ -61,9 +67,10 @@ Run `blueutil --paired' to see paired devices"
   "Connect to DEVICE if disconnected, and vice versa.
 If ACTION is \"connect\" or \"disconnect\", do nothing if already
 connected or disconnected, respectively."
-  (interactive (list (if (eq (length macos-bluetooth-device-list) 1)
-			 (caar macos-bluetooth-device-list)
-		       (completing-read "Device: " macos-bluetooth-device-list nil t))))
+  (interactive (list (when-macos
+		      (if (eq (length macos-bluetooth-device-list) 1)
+			  (caar macos-bluetooth-device-list)
+			(completing-read "Device: " macos-bluetooth-device-list nil t)))))
   (let ((MAC (alist-get device macos-bluetooth-device-list nil nil #'string=)))
     (macos--bluetooth-device MAC device action)))
 
@@ -71,32 +78,35 @@ connected or disconnected, respectively."
   "Connect to DEVICE in MAC address if disconnected, and vice versa.
 If ACTION is \"connect\" or \"disconnect\", do nothing if already
 connected or disconnected, respectively."
-  (unless (executable-find macos-blueutil)
-    (user-error "Please install `blueutil' (https://github.com/toy/blueutil) and set `macos-blueutil' accordingly"))
-  (let* ((status (macos-bluetooth-device-status MAC))
-         (action (or action (pcase status
-			      ("connected" "disconnect")
-			      ("not connected" "connect")
-			      (_ (user-error "Unknown status: %s" status)))))
-         (device (or device MAC)))
-    (when (y-or-n-p (format "%s is currently %s. %s? "
-                            device status action))
-      (pcase (shell-command-to-string (format "%s --%s %s" macos-blueutil action MAC))
-        ("" (message "%s: %sed" device action))
-        (_ (user-error "Failed to %s %s" action device))))))
+  (when-macos
+   (unless (executable-find macos-blueutil)
+     (user-error "Please install `blueutil' (https://github.com/toy/blueutil) and set `macos-blueutil' accordingly"))
+   (let* ((status (macos-bluetooth-device-status MAC))
+          (action (or action (pcase status
+			       ("connected" "disconnect")
+			       ("not connected" "connect")
+			       (_ (user-error "Unknown status: %s" status)))))
+          (device (or device MAC)))
+     (when (y-or-n-p (format "%s is currently %s. %s? "
+                             device status action))
+       (pcase (shell-command-to-string (format "%s --%s %s" macos-blueutil action MAC))
+         ("" (message "%s: %sed" device action))
+         (_ (user-error "Failed to %s %s" action device)))))))
 
 (defun macos-bluetooth-device-status (MAC)
   "Return the connection status of bluetooth device in address MAC."
-  (let* ((output (shell-command-to-string (format "%s --info %s" macos-blueutil MAC))))
-    (string-match (format "address: %s, \\([a-z ]*connected\\)" MAC) output)
-    (match-string 1 output)))
+  (when-macos
+   (let* ((output (shell-command-to-string (format "%s --info %s" macos-blueutil MAC))))
+     (string-match (format "address: %s, \\([a-z ]*connected\\)" MAC) output)
+     (match-string 1 output))))
 
 ;;;;; sleep
 
 (defun macos-sleep ()
   "Put the system to sleep."
   (interactive)
-  (shell-command "osascript -e 'tell application \"Finder\" to sleep'"))
+  (when-macos
+   (shell-command "osascript -e 'tell application \"Finder\" to sleep'")))
 
 ;;;;; hard drive
 
@@ -105,26 +115,33 @@ connected or disconnected, respectively."
 (defun macos-eject-external-hard-drive ()
   "Eject external hard drive."
   (interactive)
-  (let ((list-external
-         (shell-command-to-string
-          "diskutil list external")))
-    (string-match
-     "/dev/disk\\([[:digit:]]\\) (external, physical):"
-     list-external)
-    (if-let ((match (match-string 1 list-external)))
-        (shell-command
-         (format
-          "diskutil eject disk%s"
-          match))
-      (message "No external hard drives found."))))
+  (when-macos  
+   (let ((list-external
+          (shell-command-to-string
+           "diskutil list external")))
+     (string-match
+      "/dev/disk\\([[:digit:]]\\) (external, physical):"
+      list-external)
+     (if-let ((match (match-string 1 list-external)))
+         (shell-command
+          (format
+           "diskutil eject disk%s"
+           match))
+       (message "No external hard drives found.")))))
 
 ;;;;; open apps
 
 (defun macos-app-is-open-p (app)
   "Return t iff APP is open on macOS."
-  (let ((script (format "osascript -e 'application \"%s\" is running'" app)))
-    (string= "true"
-             (string-trim (shell-command-to-string script)))))
+  (when-macos
+   (let ((script (format "osascript -e 'application \"%s\" is running'" app)))
+     (string= "true"
+	      (string-trim (shell-command-to-string script))))))
+
+(defun macos-open-app (app)
+  "Open macOS APP."
+  (when-macos 
+   (shell-command (format "open -a %s" app))))
 
 (provide 'macos)
 ;;; macos.el ends here
